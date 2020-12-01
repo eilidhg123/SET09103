3b820be0cfe0868a9f5487669ebeee278abc0ae7
eilidhg123 <40345238@live.napier.ac.uk>
2020-12-01 13:45:18 +0000

    fixed errors and linked pages together

--- Coursework/index.py
+++ Coursework/index.py
@@ -4,46 +4,22 @@ app = Flask(__name__)
 @app.route("/", methods=['POST','GET'])
 def home():
     #home page
-    if request.method=='POST':
-        print(request.form)
-        level=request.form['level']
-        return "Hopefully this works" % level
-    else:
-        return render_template('home.html')
+    return render_template('home.html')
 
 @app.route("/stageone/", methods=['POST','GET'])
 def stageone():
-    #stage one pagei
-    if request.method=='POST':
-        print(request.form)
-        level=request.form['level']
-        return "look you mad it to" % level
-    else:
-        return redirect(url_for('home'))
+    #stage one page
+    return render_template('stageone.html')
 
 @app.route("/stagetwo/", methods=['POST','GET'])
 def stagetwo():
     #stage two page
-    if request.method=='POST':
-        print(request.form)
-        level=request.form['level']
-        return "bruh this needs an actual page here" % level
-    else:
-        return "This is where stage two lives"
+    return render_template('stagetwo.html')
 
 @app.errorhandler(404)
-def pagenotfound():
+def pagenotfound(error):
     #a catch for pages and paths that don't exist
     return "Error, page doesn't exist"
-
-@app.route('/config/')
-def config():
-    s=[]
-    s.append('debug:'+app.config['DEBUG'])
-    s.append('port:'+app.config['port'])
-    s.append('url:'+app.config['url'])
-    s.append('ip_address:'+app.config['ip_address'])
-    return ', '.join(s)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
